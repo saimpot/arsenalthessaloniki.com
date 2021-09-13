60f742bef3527a9b07ed0541e263e53f516e7577
saimpot <saimpot@gmail.com>
2021-09-13 15:50:06 +0300

    Added some assets and worked on the navbar

--- wp-content/themes/wenger-lives-on/index.php
+++ wp-content/themes/wenger-lives-on/index.php
@@ -14,80 +14,30 @@
 
 get_header();
 ?>
-    <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
-        <div class="container">
-            <div class="navbar-brand">
-                <a class="navbar-item" href="https://bulma.io">
-                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-                </a>
-
-                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-            </div>
-
-            <div id="navbarBasicExample" class="navbar-menu">
-                <div class="navbar-start">
-                    <a class="navbar-item">
-                        Home
+    <div class="styled-top-bar"></div>
+    <nav class="navbar" role="navigation" aria-label="main-navigation">
+        <div class="navbar-inner">
+            <div class="container">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="<?php echo get_bloginfo('siteurl'); ?>" title="<?php echo get_bloginfo('name'); ?> - <?php echo get_bloginfo('description'); ?>" class="navbar-item">
+                        <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/logo.png'; ?>" alt="<?php echo get_bloginfo('name'); ?> - <?php echo get_bloginfo('description'); ?>">
                     </a>
-
-                    <a class="navbar-item">
-                        Documentation
-                    </a>
-
-                    <div class="navbar-item has-dropdown is-hoverable">
-                        <a class="navbar-link">
-                            More
-                        </a>
-
-                        <div class="navbar-dropdown">
-                            <a class="navbar-item">
-                                About
-                            </a>
-                            <a class="navbar-item">
-                                Jobs
-                            </a>
-                            <a class="navbar-item">
-                                Contact
-                            </a>
-                            <hr class="navbar-divider">
-                            <a class="navbar-item">
-                                Report an issue
-                            </a>
-                        </div>
-                    </div>
                 </div>
 
-                <div class="navbar-end">
-                    <div class="navbar-item">
-                        <div class="buttons">
-                            <a class="button is-primary">
-                                <strong>Sign up</strong>
-                            </a>
-                            <a class="button is-light">
-                                Log in
-                            </a>
+                <div class="navbar-menu">
+                    <div class="navbar-start"><a href="asdf" class="navbar-item">Left link</a></div>
+                    <div class="navbar-end">
+                        <div class="navbar-item">
+                            <a href="asdf" class="button is-link is-small cta">Gine melos</a>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="navbar-team-rankings"></div>
+        <div class="navbar__bg-image" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/assets/images/theme-bg.jpg'; ?>);">
+            <div class="navbar__bg-overlay"></div>
         </div>
     </nav>
-
-    <section class="hero is-info">
-        <div class="container">
-            <div class="hero-body">
-                <p class="title">
-                    Primary hero
-                </p>
-                <p class="subtitle">
-                    Primary subtitle
-                </p>
-            </div>
-        </div>
-    </section>
 <?php
 get_footer();
